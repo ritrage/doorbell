0c8238e23d12340b053cb7fc0b97bbaf7a786dc7
Ritwik Sharma <sharmaritwik98@gmail.com>
2023-09-03 05:13:50 +0530

    env added

--- src/main/java/com/ritrageproduction/doorbell/util/Constants.java
+++ src/main/java/com/ritrageproduction/doorbell/util/Constants.java
@@ -2,10 +2,9 @@ package com.ritrageproduction.doorbell.util;
 
 public class Constants {
 	
-	public static final String toPhone = "+917987508826";
-	public static final String fromPhone = "+19259488660";
-	public static final String toPhone1 = "+919691985176";
-	public static final String AUTH_TOKEN = "9b9f83b322ab84c30a948a3bb25fb801";
-	public static final String ACCOUNT_SID = "AC10d6e812182f78379712436fffe5d888";
+	public static final String toPhone = "toPhone";
+	public static final String fromPhone = "fromPhone";
+	public static final String AUTH_TOKEN = "AUTH_TOKEN";
+	public static final String ACCOUNT_SID = "ACCOUNT_SID";
 
 }
